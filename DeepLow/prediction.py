acc3f3cbc152690852ccadd442a2a9394989790a
Merve Nur Güler <72803135+MerveNurGuler@users.noreply.github.com>
2024-07-17 14:52:38 +0300

    Update prediction.py

--- DeepLow/prediction.py
+++ DeepLow/prediction.py
@@ -3,7 +3,7 @@ import time
 import os
 import numpy as np
 
-from deep_mismatch.cnn_model import CNN, pre_process_data
+from DeepLow.cnn_model import CNN, pre_process_data
 
 label_map = {
     0: "2nd degree",
